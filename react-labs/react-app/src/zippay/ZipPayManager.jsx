030aaccfb897f03ff97a597ebe41a0a7fa9a2a41
John Paxton <johnpaxton@gmail.com>
2024-04-19 11:35:10 -0400

    Add React Labs

---
+++ react-labs/react-app/src/zippay/ZipPayManager.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './Navbar';
+import './zippay.css';
+
+function ZipPayManager() {
+	return (
+		<section className="zippay-main">
+			<Navbar></Navbar>
+		</section>
+	);
+}
+
+export default ZipPayManager;
