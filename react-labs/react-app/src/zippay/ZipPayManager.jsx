cc597a55f5272158ce114bcb271b31930a4f8f85
John Paxton <johnpaxton@gmail.com>
2024-04-23 12:00:20 -0400

    Forgot one last thing

--- react-labs/react-app/src/zippay/ZipPayManager.jsx
+++ react-labs/react-app/src/zippay/ZipPayManager.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import Navbar from './Navbar';
 import './zippay.css';
-import PaymentsGrid from './payments-grid/PaymentsGridManager';
 
 function ZipPayManager() {
 	return (
 		<section className="zippay-main">
 			<Navbar></Navbar>
-			<PaymentsGrid />
 		</section>
 	);
 }
