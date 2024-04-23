8b5ad226b0bcd0b1e4aa815fbc9bf4b58c0b2257
John Paxton <johnpaxton@gmail.com>
2024-04-23 11:37:48 -0400

    CSS and organizational changes

--- react-labs/react-app/src/zippay/ZipPayManager.jsx
+++ react-labs/react-app/src/zippay/ZipPayManager.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './Navbar';
 import './zippay.css';
+import PaymentsGrid from './payments-grid/PaymentsGridManager';
 
 function ZipPayManager() {
 	return (
 		<section className="zippay-main">
 			<Navbar></Navbar>
+			<PaymentsGrid />
 		</section>
 	);
 }
