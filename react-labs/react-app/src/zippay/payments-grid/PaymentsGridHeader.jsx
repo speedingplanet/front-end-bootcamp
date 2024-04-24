936436cffd8c7654d9b3b1df1f2eef85803bf93d
John Paxton <johnpaxton@gmail.com>
2024-04-24 13:38:30 -0400

    Starting the sort implementation

--- react-labs/react-app/src/zippay/payments-grid/PaymentsGridHeader.jsx
+++ react-labs/react-app/src/zippay/payments-grid/PaymentsGridHeader.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 
 function PaymentsGridHeader({ columns }) {
+	function handleHeaderClick(field) {
+		console.log(`Clicked on the ${field} header`);
+	}
+
 	return (
 		<div className="pgHeader">
 			{columns.map((column) => {
 				if (column.visible) {
-					return <div key={column.field}>{column.label}</div>;
+					return (
+						<div
+							onClick={() => handleHeaderClick(column.field)}
+							key={column.field}
+						>
+							{column.label}
+						</div>
+					);
 				}
 				return null;
 			})}
