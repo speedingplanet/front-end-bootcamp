518b8584c7534ea1d002e059c4e130e27a3b0db6
John Paxton <johnpaxton@gmail.com>
2024-04-25 17:12:50 -0400

    User Details complete

--- react-labs/react-app/src/zippay/UserDetails.jsx
+++ react-labs/react-app/src/zippay/UserDetails.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
-function UserDetails({ userId }) {
+function UserDetails({ user }) {
 	return (
-		<div>
-			<p>User Details for user id: {userId}</p>
+		<div className="user-details">
+			<div className="icon">
+				<img
+					src={`https://robohash.org/${user.userId}`}
+					alt="Robot Avatar"
+				/>
+			</div>
+			<div className="fullName">
+				{user.firstName} {user.lastName}
+			</div>
+			<div className="email">{user.email}</div>
+			<div className="address">
+				{user.city}, {user.province} {user.postalCode}
+			</div>
+			<div className="userName">{user.userId}</div>
 		</div>
 	);
 }
--- react-labs/react-app/src/zippay/UserDetailsRouter.jsx
+++ react-labs/react-app/src/zippay/UserDetailsRouter.jsx
@@ -1,19 +1,29 @@
 import React from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import UserDetails from './UserDetails';
+import users from '../../data/users.json';
 
 function UserDetailsRouter() {
 	let params = useParams();
 	let navigate = useNavigate();
+
+	let user = users.find((u) => u.userId === params.userId);
+
+	if (!user) {
+		return <div>User {params.userId} not found</div>;
+	}
+
 	return (
 		<section>
-			<UserDetails userId={params.userId} />
-			<button
-				className="btn btn-info"
-				onClick={() => navigate(-1)}
-			>
-				Back
-			</button>
+			<UserDetails user={user} />
+			<div style={{ marginTop: '5px' }}>
+				<button
+					className="btn btn-info"
+					onClick={() => navigate(-1)}
+				>
+					Back
+				</button>
+			</div>
 		</section>
 	);
 }
